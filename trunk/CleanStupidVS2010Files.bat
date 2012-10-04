08d87421f054bbdb3686f62b4b9352075259df26
overmind.900@gmail.com <overmind.900@gmail.com@91704b81-6cf4-e05e-ed9f-00668ac0e4d9>
2012-10-04 23:11:00 +0000

    Created Project Solution

---
+++ trunk/CleanStupidVS2010Files.bat
@@ -0,0 +1,14 @@
+del *.sdf /s
+del *.user /s
+rd Debug /s /q
+rd ipch /s /q
+del *.manifest /s
+del *.obj /s
+del *.tlog /s
+del *.pdb /s
+del *.rc /s
+del *.lastbuildstate /s
+del *.log /s
+del *.idb /s
+del *.manifest.res /s
+del *.opensdf /s
