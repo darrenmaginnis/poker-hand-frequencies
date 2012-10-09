8ddcb081a6f91625b9863156c5f2e116c4bcf144
r.johnston2281@gmail.com <r.johnston2281@gmail.com@91704b81-6cf4-e05e-ed9f-00668ac0e4d9>
2012-10-09 15:27:03 +0000

    added main.cpp

---
+++ poker-hand-frequencies/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+
+	string hearts [13] = {"2H","3H","4H","5H","6H","7H","8H","9H","10H","JH","QH","KH","AH"};
+	string clubs [13] = {"2C","3C","4C","5C","6C","7C","8C","9C","10C","JC","QC","KC","AC"};
+	string spades [13] = {"2S","3S","4S","5S","6S","7S","8S","9S","10S","JS","QS","KS","AS"};
+	string diamonds [13] = {"2D","3D","4D","5D","6D","7D","8D","9D","10D","JD","QD","KD","AD"};
+
+	string hands [10] = {"Royal Flush","Straight Flush","Four of a Kind","Full House","Flush","Straight","Three of a Kind","Two Pair","Pair","High Card"};
+
+	
+
+	return 0;
+}
