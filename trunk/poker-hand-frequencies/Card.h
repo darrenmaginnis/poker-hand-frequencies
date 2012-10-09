198366b5b325c01d3d8ab857478d6dce9e515aa7
overmind.900@gmail.com <overmind.900@gmail.com@91704b81-6cf4-e05e-ed9f-00668ac0e4d9>
2012-10-09 18:39:17 +0000

    changes to the Card class

--- trunk/poker-hand-frequencies/Card.h
+++ trunk/poker-hand-frequencies/Card.h
@@ -1,6 +1,8 @@
 #ifndef __CARD_H__
 #define __CARD_H__
+
 #include <cstdlib>
+#include <utility>
 
 class Card
 {
@@ -8,30 +10,44 @@ public:
 	enum Suit{ Spades = 0, Hearts, Diamonds, Clubs };
 	enum Rank { Two = 0, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
 
+	Suit CardSuit;
+	Rank CardRank;
 
 	// Function name   : Card
 	// Description     : Generates A Random Card
 	// Return type     : Card
 	// Argument        : void
-	Card(void);
+	Card(void)
+	{
+		CardSuit =  static_cast<Suit>(rand() % 4);
+		CardRank =  static_cast<Rank>(rand() % 13);
+	}
 	
 	// Function name   : Card
 	// Description     : Generates a Card with the supplyed Suit and Rank
 	// Return type     : Card
 	// Argument        : Suit s
 	// Argument        : Rank r
-	Card(Suit s, Rank r);
+	Card(Suit s, Rank r)
+	{
+		CardRank = r;
+		CardSuit = s;
+	}
 
 	// Function name   : ~Card
 	// Description     : Card Destructor
 	// Argument        : void
 	~Card(void);
 
-	Suit GetSuit();
-	Rank GetRank();
-private:
-	Suit m_CardSuit;
-	Rank m_CardRank;
 };
+
+bool operator < (Card const& lhs, Card const& rhs ){
+	return static_cast<int>(lhs.CardRank) < static_cast<int>(rhs.CardRank);
+}
+
+bool operator == (Card const& lhs, Card const& rhs ){
+	return (static_cast<int>(lhs.CardRank) == static_cast<int>(rhs.CardRank)) 
+		&& (static_cast<int>(lhs.CardSuit) == static_cast<int>(rhs.CardSuit));
+}
 	
 #endif //__CARD_H__
